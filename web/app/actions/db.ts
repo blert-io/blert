24c006d16b7f69dc1380bec2400561bc9e5a56fc
Alexei Frolov <alexeifrolov3@gmail.com>
2024-05-31 15:55:33 -0700

    Require SSL postgres connections in production

--- web/app/actions/db.ts
+++ web/app/actions/db.ts
@@ -7,5 +7,10 @@ if (['development', 'test'].includes(process.env.NODE_ENV)) {
     debug: (_, query, params) => console.log(query, params),
   };
 }
+if (process.env.NODE_ENV === 'production') {
+  connectionOptions = {
+    ssl: 'require',
+  };
+}
 
 export const sql = postgres(process.env.BLERT_DATABASE_URI!, connectionOptions);
