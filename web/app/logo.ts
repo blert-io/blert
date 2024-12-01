da18d7f394d9e440d790bc8aeb3b581cfe9f0741
Alexei Frolov <alexeifrolov3@gmail.com>
2024-12-01 10:44:52 -0800

    Update seasonal logo

--- web/app/logo.ts
+++ web/app/logo.ts
@@ -1,6 +1,7 @@
 enum Logos {
   STANDARD = '/images/blert.png',
+  CHRISTMAS = '/images/blert-christmas.png',
   HALLOWEEN = '/images/blert-halloween.png',
 }
 
-export const MAIN_LOGO = Logos.STANDARD;
+export const MAIN_LOGO = Logos.CHRISTMAS;
