d653056fdfb0dc396116511d1f8e56c593ad5315
Alexei Frolov <alexeifrolov3@gmail.com>
2025-11-25 11:33:14 -0800

    Allow configuring max postgres connections in prod

--- web/app/actions/db.ts
+++ web/app/actions/db.ts
@@ -1,22 +1,41 @@
 import postgres, { Sql, TransactionSql } from 'postgres';
 
-let connectionOptions: postgres.Options<NonNullable<unknown>> | undefined =
-  undefined;
+function initPostgres(): Sql<NonNullable<unknown>> {
+  if (!process.env.BLERT_DATABASE_URI) {
+    throw new Error('BLERT_DATABASE_URI is not set');
+  }
 
-if (process.env.NODE_ENV === 'development') {
-  connectionOptions = {
-    debug: (_, query, params) => console.log(query, params),
-    idle_timeout: 15,
-    max_lifetime: 1,
-  };
-}
-if (process.env.NODE_ENV === 'production' && !process.env.CI) {
-  connectionOptions = {
-    ssl: 'require',
-  };
+  let connectionOptions: postgres.Options<NonNullable<unknown>> | undefined =
+    undefined;
+
+  if (process.env.NODE_ENV === 'development') {
+    connectionOptions = {
+      debug: (_, query, params) => console.log(query, params),
+      idle_timeout: 15,
+      max_lifetime: 1,
+    };
+  }
+
+  if (process.env.NODE_ENV === 'production' && !process.env.CI) {
+    const maxConnections = process.env.BLERT_DATABASE_MAX_CONNECTIONS
+      ? Number.parseInt(process.env.BLERT_DATABASE_MAX_CONNECTIONS)
+      : 5;
+    connectionOptions = {
+      ssl: 'require',
+      max: maxConnections,
+      idle_timeout: 60,
+    };
+  }
+
+  return postgres(process.env.BLERT_DATABASE_URI, connectionOptions);
 }
 
-export const sql = postgres(process.env.BLERT_DATABASE_URI!, connectionOptions);
+declare global {
+  var _sql: Sql<NonNullable<unknown>> | undefined;
+}
+
+globalThis._sql ??= initPostgres();
+export const sql = globalThis._sql;
 
 export type Db =
   | Sql<NonNullable<unknown>>
