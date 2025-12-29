846a04f471a83656151a7c9b6a9c29ca9026dad3
Dylan <toccidylan@gmail.com>
2025-12-29 01:27:22 -0500

    commit to fix import

--- web/app/components/left-nav/left-nav.tsx
+++ web/app/components/left-nav/left-nav.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { Suspense } from 'react';
+import { Suspense, useContext } from 'react';
 
 import { MAIN_LOGO } from '@/logo';
 import { NavbarContext } from '@/display';
